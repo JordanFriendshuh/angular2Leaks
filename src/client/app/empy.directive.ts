83752ca17543c9f920f09618edaacf157aef03fc
Jordan Friendshuh <jordan.friendshuh@gmail.com>
2016-10-10 09:51:26 -0600

    remove dead comments

--- src/client/app/empy.directive.ts
+++ src/client/app/empy.directive.ts
@@ -1,6 +1,3 @@
-//https://github.com/k3nsei/angular2-in-viewport/blob/master/in-viewport.directive.ts
-//With edits from me
-//Padding item MUST HAVE infiniteScrollPadding in it's id
 import { Directive, OnInit, OnDestroy, ElementRef, Output, Input, EventEmitter } from '@angular/core';
 
 @Directive({
@@ -20,9 +17,3 @@ export class empty implements OnInit, OnDestroy {
   }
 
 }
-/**
- * @summary
- * @module
- * @since 10/10/16
- * @copyright Copyright (c) 2016 NETSCOUT
- */
